7b2097e62d1710f4b8b37c02f9b2561f520ad3b5
hesamazj <93445298+hesamazj@users.noreply.github.com>
2023-04-25 20:25:12 -0700

    Babbling code added
    babbling code added

---
+++ Babbling_code.py
@@ -0,0 +1,52 @@
+import RTBridge as rtb
+import numpy as np
+from functions import *
+import datetime
+
+
+
+# Babbling
+Experiment_ID = 'v0_02'
+babbling_duration = 64
+fs = 100
+minimum_input = 0.05
+maximum_input = 1
+steps = 1
+
+# RTBridge Setup
+
+pxiWin10 = "169.254.172.223:5555"
+pubPort = "5557"
+bridge = rtb.BridgeSetup(pubPort, pxiWin10, rtb.setups.hand_4_4, milliTimeStep= int(10))
+
+#Activations
+
+activations = systemID_input_gen_func2(babbling_duration,fs,steps, minimum_input,maximum_input)
+np.savetxt('./Activations/Experiment_'+Experiment_ID+'_activations.txt',activations)
+#plt.plot(activations)
+#plt.show()  
+
+
+# Connection
+bridge.startConnection()
+
+_ = bridge.sendAndReceive([0.05]*4, 2)
+start = datetime.datetime.now()
+
+# Send Data
+for activation_set in activations:
+    _ = bridge.sendAndReceive(activation_set)
+    a = 2
+end = datetime.datetime.now()
+
+end = end.minute*60 + end.second
+start = start.minute*60 + start.second
+time_differnce = end - start
+
+print(time_differnce)
+
+_ = bridge.sendAndReceive([0.05]*4, 2)
+
+print("\n\nTest has completed")
+
+
